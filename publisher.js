e5d15aed90bb689a4d9d0f275e291d4c6772da4a
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2022-01-26 00:29:25 +0300

    Create the queue and send data

--- publisher.js
+++ publisher.js
@@ -4,16 +4,30 @@ const message = {
   description: "This is a test message",
 };
 
+const data = require("./data.json");
+
+const queueName = process.argv[2] || jobsQueue;
 connect_rabbitmq();
 
 async function connect_rabbitmq() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672"); // connect to rabbitmq
     const channel = await connection.createChannel(); // create a channel
-    const assertion = await channel.assertQueue("jobsQueue"); // create a queue
+    const assertion = await channel.assertQueue(queueName); // create a queue
 
-    channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message))); // send message to queue
-    console.log("Message sent to queue", message);
+    data.forEach((i) => {
+      message.description = i.id;
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message))); // send message to queue
+      console.log("Message sent to queue", i.id);
+    });
+
+    //Interval
+    /* setInterval(() => {
+      message.description = new Date().getTime();
+
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message))); // send message to queue
+      console.log("Message sent to queue", message);  
+    }, 500); */
   } catch (error) {
     console.log(error);
   }
